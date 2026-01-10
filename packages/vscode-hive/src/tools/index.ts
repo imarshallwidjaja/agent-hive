74bf32a748dbfb4e75b57c438e9f9415d84f8710
tctinh <tctinh@tma.com.vn>
2026-01-10 16:58:32 +0700

    hive(12-register-subtask-domain-tools-5-tools): Registered 5 Subtask domain tools: hive_subtask_cr

---
+++ packages/vscode-hive/src/tools/index.ts
@@ -0,0 +1,6 @@
+export { registerTool, registerAllTools, createToolResult } from './base';
+export type { ToolInput, ToolRegistration } from './base';
+export { getFeatureTools } from './feature';
+export { getPlanTools } from './plan';
+export { getTaskTools } from './task';
+export { getSubtaskTools } from './subtask';
